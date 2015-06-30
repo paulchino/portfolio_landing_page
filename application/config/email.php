f21c5f08027e5b01167afc7572486bb313bb45aa
paulchino <paulchino@gmail.com>
2015-06-30 11:44:28 -0700

    email

--- application/config/email.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['protocol']  = "";
-$config['smtp_host'] = "";
-$config['smtp_user'] = "";
-$config['smtp_pass'] = "";
-$config['smtp_port'] = "";
-
-//end of email.php
--- application/controllers/main.php
+++ application/controllers/main.php
@@ -22,6 +22,7 @@ class Main extends CI_Controller {
 
 	public function mail()
 	{
+		//var_dump($this->input->post());
 		// Check for empty fields
 		if(empty($_POST['name'])  		||
 		   empty($_POST['email']) 		||
@@ -32,24 +33,46 @@ class Main extends CI_Controller {
 			echo "No arguments Provided!";
 			return false;
 		   }
-			
-		$name = $_POST['name'];
-		$email_address = $_POST['email'];
-		$phone = $_POST['phone'];
-		$message = $_POST['message'];
+
+		$name 			= $this->input->post('name');
+		$email_address 	= $this->input->post('email');
+		$phone 			= $this->input->post('phone');
+		$message 		= $this->input->post('message');
+
+		$email_subject = "Website Contact Form:  $name";
+		$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";			
+
+		$this->load->library('email');
+		$this->email->from('thegifgallery@gmail.com', $name);
+		$this->email->to('paulchino@gmail.com');
+		$this->email->subject($email_subject);
+		$this->email->message($email_body);
+
+		if ($this->email->send() )
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+
+		// $name = $_POST['name'];
+		//$email_address = $_POST['email'];
+		//$phone = $_POST['phone'];
+		//$message = $_POST['message'];
+
+
 
 		// Create the email and send the message
-		$to = 'pc.chang@rocketmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-		$email_subject = "Website Contact Form:  $name";
-		$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-		$headers = "From: paulchang04@gmail.com\n" . // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-					"Reply-To: $email_address";	
-		mail($to,$email_subject,$email_body,$headers);
-		return true;		
+		// $to = 'pc.chang@rocketmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+		// $email_subject = "Website Contact Form:  $name";
+		// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+		// $headers = "From: paulchang04@gmail.com\n" . // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+		// 			"Reply-To: $email_address";	
+		// $test = mail($to,$email_subject,$email_body,$headers);
+		// return $test;		
 	}
-
-
-
 }
 
 //end of main controller
